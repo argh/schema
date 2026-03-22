11abfc33ac4a9d505055a857b9addc19a45672b9
Jolly Roger <argh@v0.net>
2026-03-22 10:07:09 -0700

    comment to debug-sink to describe purpose

--- src/helpers/debug-sink.js
+++ src/helpers/debug-sink.js
@@ -2,7 +2,12 @@
 const DEBUG_SINK = Symbol.for('@versionzero/debug-sink');
 
 /**
+ * A hook for compatible loggers (e.g., `@versionzero/logger`) to automatically capture debug messages.
+ * (Left in for new code hacking; debug calls were removed for performance reasons after the library was stabilized.)
+ *
  * @param {...any} args
+ * @package
+ * @deprecated
  */
 export function debug(...args) {
   const sink = (globalThis)[DEBUG_SINK] ?? console.log;
